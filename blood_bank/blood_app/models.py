a140e9d0d2c946e2e9e09ead8b9deb6cbf19ff79
nazmulmilon <milonsarker73@gmail.com>
2023-01-25 15:12:34 +0600

    blood_app and systems app added

--- blood_bank/blood_app/models.py
+++ blood_bank/blood_app/models.py
@@ -1,3 +1,23 @@
+
 from django.db import models
+from systems.models import BaseModel
+from systems.enums import BloodGroupType, UserRole, GenderType
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class UserProfile(BaseModel):
+    user = models.OneToOneField(User, models.CASCADE, help_text="donor's user name")
+    phone_no = models.CharField(max_length=13, help_text="donor's mobile number")
+    blood_group = models.CharField(max_length=10, choices=BloodGroupType.choices(),
+                                   default=BloodGroupType.B_POSITIVE.value)
+    user_type = models.CharField(max_length=50, choices=UserRole.choices(), default=UserRole.DONOR.value)
+    last_donation_date = models.DateField(auto_now_add=False)
+    date_of_birth = models.DateField(auto_now_add=False)
+    donation_area = models.CharField(max_length=100, help_text="donor's donation area")
+    permanent_address = models.CharField(max_length=100, help_text="donor's permanent address")
+    gender = models.CharField(max_length=10, choices=GenderType.choices(), default=GenderType.MALE.value)
+
+    class Meta:
+        db_table = 'user_profiles'
--- blood_bank/blood_bank/settings.py
+++ blood_bank/blood_bank/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'systems',
+    'blood_app',
 ]
 
 MIDDLEWARE = [
